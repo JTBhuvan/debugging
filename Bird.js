80025bf4d3bde005a17dfed6b567e4073a444107
JTBhuvan <jairaj.tup@gmail.com>
2020-01-12 10:42:29 +0530

    debugging

--- Bird.js
+++ Bird.js
@@ -9,7 +9,7 @@ class Bird extends BaseClass {
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-
+    console.log(this.body.speed);
     super.display();
 
     if(this.body.velocity.x > 10 && this.body.position.x > 200){
